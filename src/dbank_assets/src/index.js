380301e6a67dbc0e75b70d4e2c42b091288cfd72
Thanseer. T <tthanseer407@gmail.com>
2023-08-28 21:05:20 +0530

    Index.js, html,css pending

---
+++ src/dbank_assets/src/index.js
@@ -0,0 +1,43 @@
+import {dbank} from "../../declarations/dbank";
+
+window.addEventListener("load", async function (){
+  // console.log("finish loading");
+  update();
+});
+
+
+document.querySelector("form").addEventListener("submit", async function(event){
+  event.preventDefault();
+  // console.log("clicked");
+
+const button = event.target.querySelector("#submit-btn");
+
+const inputAmount = parseFloat(document.getElementById("input-amount").value);
+const outputAmount = parseFloat(document.getElementById("withdrawal-amount").value);
+
+button.setAttribute("disabled", true);
+
+if (document.getElementById("input-amount").value !=0 ){
+  await dbank.topUp(inputAmount);
+};
+
+if(document.getElementById("withdrawal-amount").value !=0){
+  await dbank.withDraw(outputAmount);
+};
+
+await dbank.compound();
+
+update();
+
+document.getElementById("input-amount").value="";
+document.getElementById("withdrawal-amount").value="";
+
+button.removeAttribute("disabled");
+
+
+});
+
+async function update() {
+  const currentAmount = await dbank.bankBalance();
+document.getElementById("value").innerText = Math.round(currentAmount*100)/100;
+}
